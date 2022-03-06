4219688bec5438b805981a1f791df397a17856a8
mustapha <ozovehe09@gmail.com>
2022-03-06 22:35:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ module.exports = {
   images: {
     loader: 'akamai',
     path: ''
+  },
+  exportTrailingSlash: 'true',
+  exportPathMap: async function (
+    defaultPathMap,
+    {dev, dir, outDir, distDir, buildId}
+  ){
+    return {
+      '/': {page: '/'},
+      '/about': {page: '/about'}
+    }
   }
 }
