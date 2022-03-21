d96a5dc3d9fe418e80ed403cfa5189c47b128a68
mustapha <ozovehe09@gmail.com>
2022-03-21 11:21:12 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
                     <div className="element-item"></div>
                 </div>
                 <Head>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+                    <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
                 </Head>
                 <script async src="/js/modernizr-min.js" />
                 <script async src="/js/easing-min.js" />
